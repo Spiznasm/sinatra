afe8064c3d9a5a9cc57d79b04b95ec727863807b
Sam Nixa <spiznasm@gmail.com>
2017-07-10 15:48:43 +0000

    Working number guesser

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,11 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand(100)
+SECRET_NUMBER = rand(100)
+def check_guess(number)
+  if number.nil?
+    ""
+  elsif number.to_i-SECRET_NUMBER > 5
+    "Way too high"
+  elsif number.to_i-SECRET_NUMBER > 0
+    "Too high"
+  elsif number.to_i-SECRET_NUMBER == 0
+    "YOU DID IT!\n The secret number is #{SECRET_NUMBER}."
+  elsif number.to_i-SECRET_NUMBER < -5
+    "Way too low"
+  elsif number.to_i-SECRET_NUMBER < 0
+    "Too low"
+  end
+end
 
 get '/' do
-  if params['guess'].to_i > secret_number
-    message = "Too high"
-  end
-  erb :index, :locals => {:secret_number => secret_number, :message => message}
+  message = check_guess(params['guess'])
+  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message}
 end
