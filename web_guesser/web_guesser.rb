b115c96056135f57357a4397b48efe68a08cec19
Sam Nixa <spiznasm@gmail.com>
2017-07-10 16:22:31 +0000

    Background changes color with guess

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -2,23 +2,29 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(100)
+@@background = '#ffffff'
 def check_guess(number)
   if number.nil?
     ""
   elsif number.to_i-SECRET_NUMBER > 5
+    @@background = '#ff0000'
     "Way too high"
   elsif number.to_i-SECRET_NUMBER > 0
+    @@background = '#ff9999'
     "Too high"
   elsif number.to_i-SECRET_NUMBER == 0
+    @@background = '#33cc33'
     "YOU DID IT!\n The secret number is #{SECRET_NUMBER}."
   elsif number.to_i-SECRET_NUMBER < -5
+    @@background = '#ff0000'
     "Way too low"
   elsif number.to_i-SECRET_NUMBER < 0
+    @@background = '#ff9999'
     "Too low"
   end
 end
 
 get '/' do
   message = check_guess(params['guess'])
-  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message, :background => @@background}
 end
